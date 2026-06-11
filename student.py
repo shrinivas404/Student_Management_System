6792d918cbd1be84560cb6514a98af270f62425b
shrinivas404 <shrinivasdeshpande404@gmail.com>
2026-06-11 11:02:21 +0530

    Added Student Name ID

--- student.py
+++ student.py
@@ -1,5 +1,29 @@
 students = []
 
+def add_student():
+    roll_no = input("Enter Roll Number: ")
+    name = input("Enter Name: ")
+    department = input("Enter Department: ")
+
+    student = {
+        "roll_no": roll_no,
+        "name": name,
+        "department": department
+    }
+
+    students.append(student)
+    print("Student added successfully!")
+
+def delete_student():
+    roll_no = input("Enter Roll Number to delete: ")
+
+    for student in students:
+        if student["roll_no"] == roll_no:
+            students.remove(student)
+            print("Student deleted successfully!")
+            return
+
+    print("Student not found.")
 def view_students():
     if not students:
         print("No students available.")
@@ -9,7 +33,7 @@ def view_students():
     print("-" * 30)
 
     for student in students:
-        print(f"Roll No: {student['roll']}")
+        print(f"Roll No: {student['roll_no']}")
         print(f"Name: {student['name']}")
         print(f"Department: {student['department']}")
         print("-" * 30)
@@ -17,9 +41,9 @@ def view_students():
 
 def search_student(roll_no):
     for student in students:
-        if student["roll"] == roll_no:
+        if student["roll_no"] == roll_no:
             print("\nStudent Found")
-            print(f"Roll No: {student['roll']}")
+            print(f"Roll No: {student['roll_no']}")
             print(f"Name: {student['name']}")
             print(f"Department: {student['department']}")
             return
